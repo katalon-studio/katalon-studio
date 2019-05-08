82d119e3e39e549e7907d7131e7dce55be2bf8f4
hiepdang1908 <anhtutran3007@gmail.com>
2019-05-08 09:17:30 +0700

    upload file

--- app.js
+++ app.js
@@ -1,28 +1,65 @@
-var  username = process.argv[2];
-var password = process.argv[3];
-var filelocation = process.argv[4];
-const pathToThisFolder = require('path').resolve(require('path').resolve());
-const relativePath = require('path').relative(pathToThisFolder, filelocation);
 const ghRelease = require('gh-release');
+var username = process.argv[2];
+var password = process.argv[3];
+var name = process.argv[4];
+var filelocation = process.argv.length;
+var file ="";
+if (filelocation>4){
+for(var i=5;i<filelocation;i++){
+  file = process.argv[i]; 
+  const pathToThisFolder = require('path').resolve(require('path').resolve());
+  const relativePath = [require('path').relative(pathToThisFolder, file)];
+  const options = {
+    tag_name: name,
+    target_commitish: 'master',
+    name: name,
+    body: file.replace(/^.*[\\\/]/, ''),
+    assets: relativePath,
+    draft: true,
+    prerelease: false,
+    repo: 'test-katalon',
+    owner: 'hiepdang1908',
+    endpoint: 'https://api.github.com'
+  }
+  
+  options.auth = {
+    username: username,
+    password: password,
+    filelocation: filelocation
+  }
+  
+  ghRelease(options, function (err, result) {
+    if (err) throw err
+    console.log(result);
+  })
+  
+}
+}
+else{
+  file = process.argv[5];
+  const pathToThisFolder = require('path').resolve(require('path').resolve());
+const relativePath = require('path').relative(pathToThisFolder, file);
 const options = {
-     tag_name: 'v1.0.0',
-     target_commitish: 'master',
-     name: 'v1.0.0',
-     body: filelocation.replace(/^.*[\\\/]/, '') ,
-     assets: [relativePath],
-     draft: true,
-     prerelease: false,
-     repo: 'katalon-studio',
-     owner: 'katalon-studio',
-     endpoint: 'https://api.github.com'
-   }
-   options.auth = {
-     username: username,
-     password: password,
-     filelocation: filelocation
-   }
-    
-   ghRelease(options, function (err, result) {
-     if (err) throw err
-     console.log(result);
-   })
+  tag_name: name,
+  target_commitish: 'master',
+  name: name,
+  body: file.replace(/^.*[\\\/]/, ''),
+  assets: [relativePath],
+  draft: true,
+  prerelease: false,
+  repo: 'test-katalon',
+  owner: 'hiepdang1908',
+  endpoint: 'https://api.github.com'
+}
+
+options.auth = {
+  username: username,
+  password: password,
+  filelocation: filelocation
+}
+
+ghRelease(options, function (err, result) {
+  if (err) throw err
+  console.log(result);
+})
+}
